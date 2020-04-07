0b86f77cea826da2d9a45d3eaff35f5d14dd4344
byatso <57915292+byatso@users.noreply.github.com>
2020-04-07 22:47:22 +0000

    Update claim.php

--- claim.php
+++ claim.php
@@ -1,4 +1,3 @@
-
 <?php
 date_default_timezone_set('Asia/Jakarta');
 include "function.php";
@@ -53,7 +52,7 @@ function change(){
         echo color("yellow",".");
         sleep(20);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420A"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
